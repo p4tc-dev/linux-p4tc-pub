3e5c6c64712d720def764aff0d88fc91e3a62f3d
Victor Nogueira <victor@mojatatu.com>
2022-07-25 19:48:45 +0000

    net/sched: act_api: Add init_ops to struct tc_action_op
    As with all TC actions, dynamic actions possess a pernet structure which
    holds the IDR where the action instances are stored. In the regular
    actions (such as gact, mirred and ife), the net ID necessary to access this
    pernet structure is static to the file where they are defined, which
    means this ID is available to every function declared in the file. In
    the case of dynamic functions, this ID can't be static to the file,
    because we'll need a new ID for each new dynamic function. With that in
    mind, we needed to find a way to pass this ID when needed, such as when the
    init or the lookup callbacks are executed. To solve this issue, we
    added the netid to struct tc_action_op and two new callbacks to the
    struct tc_action_ops: init_ops and lookup_ops, which have, respectively,
    the same prototype as init and lookup, but with the op as an extra
    parameter. Also we needed to add a new refcount for dynamic functions
    called dyn_ref.
    
    XXX: This commit should disappear completely
    
    Signed-off-by: Victor Nogueira <victor@mojatatu.com>
    Signed-off-by: Pedro Tammela <pctammela@mojatatu.com>

--- net/sched/act_api.c
+++ net/sched/act_api.c
@@ -951,7 +951,7 @@ int tcf_register_action(struct tc_action_ops *act,
 {
 	int ret;
 
-	if (!act->act || !act->dump || !act->init)
+	if (!act->act || !act->dump || (!act->init && !act->init_ops))
 		return -EINVAL;
 
 	/* We have to register pernet ops before making the action ops visible,
@@ -1403,8 +1403,13 @@ struct tc_action *tcf_action_init_1(struct net *net, struct tcf_proto *tp,
 			}
 		}
 
-		err = a_o->init(net, tb[TCA_ACT_OPTIONS], est, &a, tp,
-				userflags.value | flags, extack);
+		if (a_o->init)
+			err = a_o->init(net, tb[TCA_ACT_OPTIONS], est, &a, tp,
+					userflags.value | flags, extack);
+		else if (a_o->init_ops)
+			err = a_o->init_ops(net, tb[TCA_ACT_OPTIONS], est, &a,
+					    tp, a_o, userflags.value | flags,
+					    extack);
 	} else {
 		err = a_o->init(net, nla, est, &a, tp, userflags.value | flags,
 				extack);
