445a744bb0832dddbb5f38f4edc3dc7c43fbe417
Victor Nogueira <victor@mojatatu.com>
2022-11-19 01:43:47 +0000

    p4tc: Change P4TC_MAX_KEYSZ
    Keys may sometimes be bigger than the maximum allowed type, in our case
    u128, that means that we need to bump P4TC_MAX_KEYSZ up. We chose 512
    for now, but this value may be increased in the near future.
    
    Fixes: 2df57c838bc9
    
    Signed-off-by: Victor Nogueira <victor@mojatatu.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -326,7 +326,8 @@ struct tc_skb_ext {
 #endif
 
 #if IS_ENABLED(CONFIG_NET_P4_TC)
-#define P4TC_MAX_KEYSZ 128
+#include <uapi/linux/p4tc.h>
+
 #define HEADER_MAX_LEN 512
 #define META_MAX_LEN 512
 
