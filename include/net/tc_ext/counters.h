6cbbdec94c5b9fdaa1018203a97802223fa7f287
Pedro Tammela <pctammela@mojatatu.com>
2023-05-03 15:35:05 -0300

    Linux net-next
    Signed-off-by: Pedro Tammela <pctammela@mojatatu.com>

---
+++ include/net/tc_ext/counters.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __NET_TC_EXT_COUNTERS_H
+#define __NET_TC_EXT_COUNTERS_H
+
+#include <linux/skbuff.h>
+#include <net/p4tc_ext_api.h>
+#endif /* __NET_TC_EXT_COUNTERS_H */
