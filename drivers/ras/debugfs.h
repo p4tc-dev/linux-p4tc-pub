11e2e506c0b09b374d66fee35bbb639dd532193d
Pedro Tammela <pctammela@mojatatu.com>
2023-05-03 15:35:05 -0300

    Linux net-next
    Signed-off-by: Pedro Tammela <pctammela@mojatatu.com>

---
+++ drivers/ras/debugfs.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __RAS_DEBUGFS_H__
+#define __RAS_DEBUGFS_H__
+
+#include <linux/debugfs.h>
+
+struct dentry *ras_get_debugfs_root(void);
+
+#endif /* __RAS_DEBUGFS_H__ */
