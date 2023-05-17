cc910bc75bca2a2caceb978bdd4b50f0a48fc259
Jamal Hadi Salim <jhs@mojatatu.com>
2023-05-17 07:02:14 -0400

    p4tc: add pipeline create, get, update, delete
    __Introducing P4 TC Pipeline__
    
    This commit introduces P4 TC pipelines, which emulate the semantics of a
    P4 program/pipeline using the TC infrastructure.
    
    One can refer to P4 programs/pipelines using their names or their
    specific pipeline ids (pipeid)
    
    CRUD (Create, Read/get, Update and Delete) commands apply on a pipeline.
    
    As an example, to create a P4 program/pipeline named aP4proggie with a
    single table in its pipeline, one would use the following command from user
    space tc:
    
    tc p4template create pipeline/aP4proggie numtables 1
    
    Note that, in the above command, the numtables is set as 1; the default
    is 0 because it is feasible to have a P4 program with no tables at all.
    
    The kernel issues each pipeline a pipeline ID which could be referenced.
    The control plane can specify an ID of choice, for example:
    
    tc p4template create pipeline/aP4proggie pipeid 1 numtables 1
    
    Typically there is no good reason to specify the pipeid, but the choice
    is offered to the user.
    
    To Read pipeline aP4proggie attributes, one would retrieve those details as
    follows:
    
    tc p4template get pipeline/[aP4proggie] [pipeid 1]
    
    To Update aP4proggie pipeline from 1 to 10 tables, one would use the
    following command:
    
    tc p4template update pipeline/[aP4proggie] [pipeid 1] numtables 10
    
    Note that, in the above command, one could use the P4 program/pipeline
    name, id or both to specify which P4 program/pipeline to update.
    
    To Delete a P4 program/pipeline named aP4proggie
    with a pipeid of 1, one would use the following command:
    
    tc p4template del pipeline/[aP4proggie] [pipeid 1]
    
    Note that, in the above command, one could use the P4 program/pipeline
    name, id or both to specify which P4 program/pipeline to delete
    
    If one wished to dump all the created P4 programs/pipelines, one would
    use the following command:
    
    tc p4template get pipeline/
    
    __Pipeline Lifetime__
    
    After Create is issued, one can Read/get, Update and Delete; however
    the pipeline can only be put to use after it is "sealed".
    To seal a pipeline, one would issue the following command:
    
    tc p4template update pipeline/aP4proggie state ready
    
    Once the pipeline is sealed it cannot updated. It can be deleted and read.
    
    After a pipeline is sealed it can be put to use via the TC P4 classifier.
    For example:
    
    tc filter add dev $DEV ingress protocol any prio 6 p4 pname aP4proggie
    
    Instantiates aP4proggie in the ingress of $DEV. One could also attach it to
    a block of ports (example tc block 22) as such:
    
    tc filter add block 22 ingress protocol any prio 6 p4 pname aP4proggie
    
    Once the pipeline is attached to a device or block it cannot be deleted.
    It becomes Read-only from the control plane/user space.
    The pipeline can be deleted when there are no longer any users left.
    
    Co-developed-by: Victor Nogueira <victor@mojatatu.com>
    Signed-off-by: Victor Nogueira <victor@mojatatu.com>
    Co-developed-by: Pedro Tammela <pctammela@mojatatu.com>
    Signed-off-by: Pedro Tammela <pctammela@mojatatu.com>
    Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>

---
+++ include/net/p4tc.h
@@ -0,0 +1,109 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __NET_P4TC_H
+#define __NET_P4TC_H
+
+#include <uapi/linux/p4tc.h>
+#include <linux/workqueue.h>
+#include <net/sch_generic.h>
+#include <net/net_namespace.h>
+#include <linux/refcount.h>
+#include <linux/rhashtable.h>
+#include <linux/rhashtable-types.h>
+
+#define P4TC_DEFAULT_NUM_TABLES P4TC_MINTABLES_COUNT
+#define P4TC_DEFAULT_MAX_RULES 1
+#define P4TC_PATH_MAX 3
+
+#define P4TC_KERNEL_PIPEID 0
+
+#define P4TC_PID_IDX 0
+
+struct p4tc_dump_ctx {
+	u32 ids[P4TC_PATH_MAX];
+};
+
+struct p4tc_template_common;
+
+struct p4tc_nl_pname {
+	char                     *data;
+	bool                     passed;
+};
+
+struct p4tc_template_ops {
+	void (*init)(void);
+	struct p4tc_template_common *(*cu)(struct net *net, struct nlmsghdr *n,
+					   struct nlattr *nla,
+					   struct p4tc_nl_pname *nl_pname,
+					   u32 *ids,
+					   struct netlink_ext_ack *extack);
+	int (*put)(struct net *net, struct p4tc_template_common *tmpl,
+		   bool unconditional_purge, struct netlink_ext_ack *extack);
+	int (*gd)(struct net *net, struct sk_buff *skb, struct nlmsghdr *n,
+		  struct nlattr *nla, struct p4tc_nl_pname *nl_pname, u32 *ids,
+		  struct netlink_ext_ack *extack);
+	int (*fill_nlmsg)(struct net *net, struct sk_buff *skb,
+			  struct p4tc_template_common *tmpl,
+			  struct netlink_ext_ack *extack);
+	int (*dump)(struct sk_buff *skb, struct p4tc_dump_ctx *ctx,
+		    struct nlattr *nla, char **p_name, u32 *ids,
+		    struct netlink_ext_ack *extack);
+	int (*dump_1)(struct sk_buff *skb, struct p4tc_template_common *common);
+};
+
+struct p4tc_template_common {
+	char                     name[TEMPLATENAMSZ];
+	struct p4tc_template_ops *ops;
+	u32                      p_id;
+	u32                      PAD0;
+};
+
+extern const struct p4tc_template_ops p4tc_pipeline_ops;
+
+struct p4tc_pipeline {
+	struct p4tc_template_common common;
+	struct rcu_head             rcu;
+	struct net                  *net;
+	refcount_t                  p_ref;
+	refcount_t                  p_ctrl_ref;
+	u16                         num_tables;
+	u16                         curr_tables;
+	u8                          p_state;
+};
+
+struct p4tc_pipeline_net {
+	struct idr pipeline_idr;
+};
+
+int tcf_p4_tmpl_generic_dump(struct sk_buff *skb, struct p4tc_dump_ctx *ctx,
+			     struct idr *idr, int idx,
+			     struct netlink_ext_ack *extack);
+
+struct p4tc_pipeline *tcf_pipeline_find_byany(struct net *net,
+					      const char *p_name,
+					      const u32 pipeid,
+					      struct netlink_ext_ack *extack);
+struct p4tc_pipeline *tcf_pipeline_find_byid(struct net *net, const u32 pipeid);
+struct p4tc_pipeline *tcf_pipeline_get(struct net *net, const char *p_name,
+				       const u32 pipeid,
+				       struct netlink_ext_ack *extack);
+void __tcf_pipeline_put(struct p4tc_pipeline *pipeline);
+struct p4tc_pipeline *
+tcf_pipeline_find_byany_unsealed(struct net *net, const char *p_name,
+				 const u32 pipeid,
+				 struct netlink_ext_ack *extack);
+
+static inline int p4tc_action_destroy(struct tc_action **acts)
+{
+	int ret = 0;
+
+	if (acts) {
+		ret = tcf_action_destroy(acts, TCA_ACT_UNBIND);
+		kfree(acts);
+	}
+
+	return ret;
+}
+
+#define to_pipeline(t) ((struct p4tc_pipeline *)t)
+
+#endif
